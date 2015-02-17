35c96cf7726b189058a5457b2e1aa835eba9aac2
Mike Dillon <mike.dillon@synctree.com>
2015-02-17 06:15:40 -0800

    Use esl-erlang and debian:squeeze for R15
    Drop R15-slim since there doesn't seem to be an erlang-base for R15 in the ESL
    repository

--- update.sh
+++ update.sh
@@ -9,21 +9,24 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-packagesUrl='http://packages.erlang-solutions.com/debian/dists/wheezy/contrib/binary-amd64/Packages'
-packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
-curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
-
 for version in "${versions[@]}"; do
+	dist="$(grep '^FROM ' "$version/Dockerfile" | cut -d' ' -f2 | sed -r 's/^(ubuntu|debian)://')"
+	package="$(grep -m1 '="$ERLANG_VERSION"' "$version/Dockerfile" | cut -d= -f1)"
+	packagesUrl="http://packages.erlang-solutions.com/debian/dists/$dist/contrib/binary-amd64/Packages"
+	packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
+	curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
+
 	majorVersion="1:${version#R}"
-	fullVersion="$(grep -A10 '^Package: erlang-base$' "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
+	fullVersion="$(grep -A10 "^Package: $package\$" "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
 	if [ "$fullVersion" ]; then
+		dockerfiles="$(ls "$version/"{,slim/}Dockerfile)"
 		(
 			set -x
 			sed -ri '
 				s/(ENV ERLANG_VERSION) .*/\1 '"$fullVersion"'/g;
-			' "$version/"{,slim/}Dockerfile
+			' $dockerfiles
 		)
 	fi
-done
 
-rm "$packages"
+	rm "$packages"
+done
