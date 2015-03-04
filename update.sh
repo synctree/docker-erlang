4fbe4b5be531f847f73d85560a7a2f02d618b4a8
Mike Dillon <mike.dillon@synctree.com>
2015-03-04 10:25:38 -0800

    Add better error checking to update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -11,21 +11,25 @@ versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do
 	dist="$(grep '^FROM ' "$version/Dockerfile" | cut -d' ' -f2 | sed -r 's/^(ubuntu|debian)://')"
-	package="$(grep -m1 '="$ERLANG_VERSION"' "$version/Dockerfile" | cut -d= -f1)"
+	package="$(grep -m1 '="$ERLANG_VERSION"' "$version/Dockerfile" | cut -d= -f1 | sed -r 's/^\s+//g; s/\s+$//g')"
 	packagesUrl="http://packages.erlang-solutions.com/debian/dists/$dist/contrib/binary-amd64/Packages"
 	packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
-	curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
+	(set -x; curl -SL "${packagesUrl}.bz2" | bunzip2 > "$packages")
 
 	majorVersion="1:${version#R}"
-	fullVersion="$(grep -A10 "^Package: $package\$" "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
+	fullVersion="$(set -x; grep -A10 "^Package: $package\$" "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
 	if [ "$fullVersion" ]; then
-		dockerfiles="$(ls "$version/"{,slim/}Dockerfile)"
+		dockerfiles="$(find "$version" -name Dockerfile)"
 		(
 			set -x
 			sed -ri '
 				s/(ENV ERLANG_VERSION) .*/\1 '"$fullVersion"'/g;
 			' $dockerfiles
 		)
+	else
+		echo 2>&1 "Failed to determine full package version for package $version"
+		rm "$packages"
+		exit 1
 	fi
 
 	rm "$packages"
