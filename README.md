d1c9090ff02a774d545894f6c896f6041fb8856c
Mike Dillon <mike.dillon@synctree.com>
2015-03-05 10:13:19 -0800

    Add R17-utopic variant

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ The `synctree/erlang` image is intended for use as a base image for Erlang/OTP a
 ## Available Variants
 There are versions of this image for Erlang/OTP R15, R16, and R17. Additionally, the R16 and R17 versions have a `slim` variant that installs only `erlang-base` and `erlang-crypto` as opposed to installing the `erlang` package and all its dependencies.
 
-| Tag      | Base             | Packages      |
-|----------|------------------|---------------|
-| R15      | `debian:squeeze` | `esl-erlang`  |
-| R16      | `debian:wheezy`  | `erlang`      |
-| R16-slim | `debian:wheezy`  | `erlang-base`<br>`erlang-crypto` |
-| R17      | `debian:jessie`  | `erlang`      |
-| R17-slim | `debian:jessie`  | `erlang-base`<br>`erlang-crypto` |
+| Tag        | Base             | Packages                         |
+|------------|------------------|----------------------------------|
+| R15        | `debian:squeeze` | `esl-erlang`                     |
+| R16        | `debian:wheezy`  | `erlang`                         |
+| R16-slim   | `debian:wheezy`  | `erlang-base`<br>`erlang-crypto` |
+| R17        | `debian:jessie`  | `erlang`                         |
+| R17-slim   | `debian:jessie`  | `erlang-base`<br>`erlang-crypto` |
+| R17-utopic | `ubuntu:utopic`  | `erlang`                         |
--- update.sh
+++ update.sh
@@ -10,27 +10,28 @@ fi
 versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do
-	dist="$(grep '^FROM ' "$version/Dockerfile" | cut -d' ' -f2 | sed -r 's/^(ubuntu|debian)://')"
-	package="$(grep -m1 '="$ERLANG_VERSION"' "$version/Dockerfile" | cut -d= -f1 | sed -r 's/^\s+//g; s/\s+$//g')"
-	packagesUrl="http://packages.erlang-solutions.com/debian/dists/$dist/contrib/binary-amd64/Packages"
-	packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
-	(set -x; curl -SL "${packagesUrl}.bz2" | bunzip2 > "$packages")
+	for dockerfile in $(find "$version" -name Dockerfile); do
+		dist="$(grep '^FROM ' "$dockerfile" | cut -d' ' -f2 | sed -r 's/^(ubuntu|debian)://')"
+		package="$(grep -m1 '="$ERLANG_VERSION"' "$dockerfile" | cut -d= -f1 | sed -r 's/^\s+//g; s/\s+$//g')"
+		packagesUrl="http://packages.erlang-solutions.com/debian/dists/$dist/contrib/binary-amd64/Packages"
+		packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
+		(set -x; curl -SL "${packagesUrl}.bz2" | bunzip2 > "$packages")
 
-	majorVersion="1:${version#R}"
-	fullVersion="$(set -x; grep -A10 "^Package: $package\$" "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
-	if [ "$fullVersion" ]; then
-		dockerfiles="$(find "$version" -name Dockerfile)"
-		(
-			set -x
-			sed -ri '
-				s/(ENV ERLANG_VERSION) .*/\1 '"$fullVersion"'/g;
-			' $dockerfiles
-		)
-	else
-		echo 2>&1 "Failed to determine full package version for package $version"
+		majorVersion="1:${version#R}"
+		fullVersion="$(set -x; grep -A10 "^Package: $package\$" "$packages" | grep "^Version: ${majorVersion}\>" | cut -d' ' -f2 | sort -Vr | head -1)"
+		if [ "$fullVersion" ]; then
+			(
+				set -x
+				sed -ri '
+					s/(ENV ERLANG_VERSION) .*/\1 '"$fullVersion"'/g;
+				' "$dockerfile"
+			)
+		else
+			echo 2>&1 "Failed to determine full package version for $dockerfile"
+			rm "$packages"
+			exit 1
+		fi
+
 		rm "$packages"
-		exit 1
-	fi
-
-	rm "$packages"
+	done
 done
